cc39a69244be786b8a0bdd6fae90965baa334810
Jack O'Connor <oconnor663@gmail.com>
2012-12-17 15:02:15 -0800

    Change Xmonad shortcuts
    Alt-Enter is needed for browsers. Unbind swapMaster from there and
    rebind it to Alt-Shift-Enter. Terminal is now Alt-t.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,4 +1,23 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
-main = xmonad =<< xmobar defaultConfig { terminal = "gnome-terminal" }
+main = xmonad =<< xmobar defaultConfig
+  { keys = newKeys
+  , terminal = "gnome-terminal"
+  }
+
+defKeys    = keys defaultConfig
+delKeys x  = foldr M.delete           (defKeys x) (toRemove x)
+newKeys x  = foldr (uncurry M.insert) (delKeys x) (toAdd    x)
+toRemove x =
+  -- unbind Alt-Enter, for browsers
+  [ (modMask x, xK_Return)
+  ]
+toAdd x =
+  -- bind terminal to Alt-t, since we're going to remap Alt-Shift-Enter below
+  [ ((modMask x, xK_t), spawn "gnome-terminal")
+  -- bind swapMaster to Alt-Shift-Enter, since we unbound Alt-Enter above
+  , ((modMask x .|. shiftMask, xK_Return), windows W.swapMaster)
+  ]
