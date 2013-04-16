29d98fbae6e1ec45c921d0409f75d6f095a3e807
Jack O'Connor <oconnor663@gmail.com>
2013-04-16 00:26:18 -0700

    set default shell to /bin/zsh

--- install.sh
+++ install.sh
@@ -19,8 +19,7 @@ LINKED_FILES=(
   zshrc
 )
 
-for NAME in ${LINKED_FILES[*]}
-do
+for NAME in ${LINKED_FILES[*]} ; do
   DEST=~/".$NAME"
 
   # Copy existing dotfiles (but not symlinks) into the old directory
@@ -49,11 +48,16 @@ END
 
 mkdir -p ~/.vim-tmp
 
-if which gnome-terminal &> /dev/null
-then
+if which gnome-terminal &> /dev/null ; then
   # terminal colors
   gnome-terminal-colors-solarized/set_dark.sh
   # terminal font
   gconftool-2 -s -t bool /apps/gnome-terminal/profiles/Default/use_system_font false
   gconftool-2 -s -t string /apps/gnome-terminal/profiles/Default/font "Ubuntu Mono 15"
+fi
+
+newshell=/bin/zsh
+if [ -e $newshell ] && [ $SHELL != $newshell ]; then
+  echo Switching default shell from $SHELL to $newshell...
+  chsh -s $newshell
 fi
