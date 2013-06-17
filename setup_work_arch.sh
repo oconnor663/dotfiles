ec0d80c2bb1fb368db1e5493216421bf61dedb69
Jack O'Connor <oconnor663@gmail.com>
2013-06-17 16:21:30 -0700

    more bugfixes in setup_work_arch.sh

--- setup_work_arch.sh
+++ setup_work_arch.sh
@@ -27,6 +27,7 @@ set -exv
 # default to Python2
 mkdir -p ~/bin
 ln -sf /usr/bin/python2 ~/bin/python
+ln -sf /usr/bin/python2 ~/bin/python2.6
 
 # add [multilib] in pacman.conf
 if ! grep '^\[multilib\]$' /etc/pacman.conf ; then
@@ -43,7 +44,7 @@ fi
 install_aur_once jdk
 
 # various dependencies
-sudo pacman -S --needed --noconfirm php apache-ant
+sudo pacman -S --needed --noconfirm php apache-ant intellij-idea-community-edition
 if ! is_installed subversion-1.6 ; then
   sudo pacman -S --needed --noconfirm subversion
 fi
@@ -58,6 +59,8 @@ mkdir -p ~/devtools
 # arc
 clone_once git://github.com/facebook/libphutil.git ~/devtools/libphutil
 clone_once git://github.com/facebook/arcanist.git ~/devtools/arcanist
+# allow PHP to run outside of web directories
+sudo sed -i 's/^open_basedir = /; open_basedir = /' /etc/php/php.ini
 if $cloned ; then
   ~/devtools/libphutil/scripts/build_xhpast.sh
   ln -sf ~/devtools/arcanist/bin/arc ~/bin/arc
