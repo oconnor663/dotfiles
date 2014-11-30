1a414699254dd3f07cd05038312fe04d696aabeb
Jack O'Connor <oconnor663@gmail.com>
2014-11-29 20:36:48 -0500

    add to startup.py

--- startup.py
+++ startup.py
@@ -1,3 +1,16 @@
 print("Jack's startup.py...")
+import math
 import os
+import re
+import subprocess
 import sys
+
+try:
+    import asyncio
+except:
+    pass
+
+try:
+    from pathlib import Path
+except:
+    pass
