37c1a4637b90995f731d3a90fd8bf6a6972f2a05
Jack O'Connor <oconnor663@gmail.com>
2015-07-26 23:34:13 -0400

    use solarized colors copied from fish_config output

--- config.fish
+++ config.fish
@@ -6,52 +6,32 @@ setenv PYTHONSTARTUP $DOTFILES/startup.py
 
 if begin; [ "$TERM" = xterm ]; or [ "$TERM" = xterm-256color ]; end
   setenv SOLARIZED 1
-
   eval (dircolors -c $DOTFILES/dircolors-solarized/dircolors.ansi-dark)
 
-  ###
-  ### TODO: Factor out these colors somehow.
-  ###
-
-  set -l base03  "--bold black"
-  set -l base02  "black"
-  set -l base01  "--bold green"
-  set -l base00  "--bold yellow"
-  set -l base0   "--bold blue"
-  set -l base1   "--bold cyan"
-  set -l base2   "white"
-  set -l base3   "--bold white"
-  set -l yellow  "yellow"
-  set -l orange  "--bold red"
-  set -l red     "red"
-  set -l magenta "magenta"
-  set -l violet  "--bold magenta"
-  set -l blue    "blue"
-  set -l cyan    "cyan"
-  set -l green   "green"
-
-  # Used by fish's completion; see
-  # http://fishshell.com/docs/2.0/index.html#variables-color
-
-  set -g fish_color_normal      $base0
-  set -g fish_color_command     $base0
-  set -g fish_color_quote       $cyan
-  set -g fish_color_redirection $base0
-  set -g fish_color_end         $base0
-  set -g fish_color_error       $red
-  set -g fish_color_param       $blue
-  set -g fish_color_comment     $base01
-  set -g fish_color_match       $cyan
-  set -g fish_color_search_match "--background=$base02"
-  set -g fish_color_operator    $orange
-  set -g fish_color_escape      $cyan
-
-  # Used by fish_prompt
-
-  set -g fish_color_hostname    $cyan
-  set -g fish_color_cwd         $yellow
-  set -g fish_color_git         $green
-
+  # Copied from the output of fish_config.
+  setenv fish_color_autosuggestion 586e75
+  setenv fish_color_command 93a1a1
+  setenv fish_color_comment 586e75
+  setenv fish_color_cwd green
+  setenv fish_color_cwd_root red
+  setenv fish_color_end 268bd2
+  setenv fish_color_error dc322f
+  setenv fish_color_escape cyan
+  setenv fish_color_history_current cyan
+  setenv fish_color_match cyan
+  setenv fish_color_normal normal
+  setenv fish_color_operator cyan
+  setenv fish_color_param 839496
+  setenv fish_color_quote 657b83
+  setenv fish_color_redirection 6c71c4
+  setenv fish_color_search_match \x2d\x2dbackground\x3dpurple
+  setenv fish_color_selection \x2d\x2dbackground\x3dpurple
+  setenv fish_color_valid_path \x2d\x2dunderline
+  setenv fish_key_bindings fish_default_key_bindings
+  setenv fish_pager_color_completion normal
+  setenv fish_pager_color_description 555\x1eyellow
+  setenv fish_pager_color_prefix cyan
+  setenv fish_pager_color_progress cyan
 end
 
 alias ta='tmux attach'
@@ -81,9 +61,7 @@ function fish_prompt
   if test $last_status -ne 0
     set prompt_status " [$last_status]"
   end
-
   set -l user (whoami)@(hostname)
-
   echo -ns (set_color blue)(prompt_pwd) \
            (set_color yellow)(__fish_git_prompt " %s") \
            (set_color red) $prompt_status " " \
