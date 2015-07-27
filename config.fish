bde91d245fbe36ee4d886a58a9836c0d771f2650
Jack O'Connor <oconnor663@gmail.com>
2015-07-26 23:40:18 -0400

    factor out solarized.fish

--- config.fish
+++ config.fish
@@ -9,31 +9,20 @@ setenv PYTHONSTARTUP $DOTFILES/startup.py
 if begin; [ "$TERM" = xterm ]; or [ "$TERM" = xterm-256color ]; end
   setenv SOLARIZED 1
   eval (dircolors -c $DOTFILES/dircolors-solarized/dircolors.ansi-dark)
+  source $DOTFILES/solarized.fish
+end
 
-  # Copied from the output of fish_config.
-  setenv fish_color_autosuggestion 586e75
-  setenv fish_color_command 93a1a1
-  setenv fish_color_comment 586e75
-  setenv fish_color_cwd green
-  setenv fish_color_cwd_root red
-  setenv fish_color_end 268bd2
-  setenv fish_color_error dc322f
-  setenv fish_color_escape cyan
-  setenv fish_color_history_current cyan
-  setenv fish_color_match cyan
-  setenv fish_color_normal normal
-  setenv fish_color_operator cyan
-  setenv fish_color_param 839496
-  setenv fish_color_quote 657b83
-  setenv fish_color_redirection 6c71c4
-  setenv fish_color_search_match \x2d\x2dbackground\x3dpurple
-  setenv fish_color_selection \x2d\x2dbackground\x3dpurple
-  setenv fish_color_valid_path \x2d\x2dunderline
-  setenv fish_key_bindings fish_default_key_bindings
-  setenv fish_pager_color_completion normal
-  setenv fish_pager_color_description 555\x1eyellow
-  setenv fish_pager_color_prefix cyan
-  setenv fish_pager_color_progress cyan
+function fish_prompt
+  set -l last_status $status
+  set -l prompt_status
+  if test $last_status -ne 0
+    set prompt_status " [$last_status]"
+  end
+  set -l user (whoami)@(hostname)
+  echo -ns (set_color blue)(prompt_pwd) \
+           (set_color yellow)(__fish_git_prompt " %s") \
+           (set_color red) $prompt_status " " \
+           (set_color normal) "> "
 end
 
 alias ta='tmux attach'
@@ -55,17 +44,3 @@ alias gpr='git pull --rebase'
 alias gfra='git fetch ;and git rebase --autostash'
 alias gout='git log "@{upstream}.." --oneline'
 alias ginit='git init ;and git add -A ;and git commit -m "first commit"'
-
-
-function fish_prompt
-  set -l last_status $status
-  set -l prompt_status
-  if test $last_status -ne 0
-    set prompt_status " [$last_status]"
-  end
-  set -l user (whoami)@(hostname)
-  echo -ns (set_color blue)(prompt_pwd) \
-           (set_color yellow)(__fish_git_prompt " %s") \
-           (set_color red) $prompt_status " " \
-           (set_color normal) "> "
-end
