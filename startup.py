4d20ce3adf03e7b67ad697c9057f88feac9038d5
Jack O'Connor <oconnor663@gmail.com>
2015-10-29 15:48:55 -0400

    stop prefix matching with Up/Down in IPython

--- startup.py
+++ startup.py
@@ -16,5 +16,12 @@ except:
     pass
 
 import readline
+# Ctrl-W behavior more like Vim.
 readline.parse_and_bind('"\\C-w": backward-kill-word')
+# Prevent ctrl-p/ctrl-n/Up/Down from doing prefix searching in IPython.
+readline.parse_and_bind('"\\C-p": previous-history')
+readline.parse_and_bind('"\\C-n": next-history')
+readline.parse_and_bind('"\\e[A": previous-history')
+readline.parse_and_bind('"\\e[B": next-history')
+
 subprocess.call(['stty', 'werase', 'undef'])
