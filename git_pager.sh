bcb4484833835bdaf01b593874205f079a463298
Jack O'Connor <oconnor663@gmail.com>
2016-02-12 10:14:11 -0500

    strip leading + and - in diffs

--- git_pager.sh
+++ git_pager.sh
@@ -8,4 +8,11 @@ highlight() {
   /usr/share/git/diff-highlight/diff-highlight
 }
 
-highlight | "$here/git_format.py" | less --tabs=1,5 -R
+# Strip the leading + and - from diff output. Taken from
+# https://github.com/stevemao/diff-so-fancy.
+strip_leading_symbols() {
+    color_code_regex=$'(\x1B\\[([0-9]{1,2}(;[0-9]{1,2})?)[m|K])?'
+    sed -E "s/^$color_code_regex[\+\-]/\1 /g"
+}
+
+highlight | strip_leading_symbols | "$here/git_format.py" | less --tabs=1,5 -R
