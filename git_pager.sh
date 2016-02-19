cb529f4d78ef373f7703b0b60ea67e37b87dfe99
Jack O'Connor <oconnor663@gmail.com>
2016-02-19 09:50:01 -0500

    get rid of strip_leading_symbols in the git pager
    It made it hard to see whitespace.

--- git_pager.sh
+++ git_pager.sh
@@ -8,11 +8,4 @@ highlight() {
   /usr/share/git/diff-highlight/diff-highlight
 }
 
-# Strip the leading + and - from diff output. Taken from
-# https://github.com/stevemao/diff-so-fancy.
-strip_leading_symbols() {
-    color_code_regex=$'(\x1B\\[([0-9]{1,2}(;[0-9]{1,2})?)[m|K])?'
-    sed -E "s/^$color_code_regex[\+\-]/\1 /g"
-}
-
-highlight | strip_leading_symbols | "$here/git_format.py" | less --tabs=1,5 -R
+highlight | "$here/git_format.py" | less --tabs=1,5 -R
